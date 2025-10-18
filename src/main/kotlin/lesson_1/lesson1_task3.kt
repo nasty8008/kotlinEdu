31d5d6b073593796f8b95013e10bbddf585bcefe
nasty8008 <nastya901296@gmail.com>
2025-10-18 19:43:18 +0400

    KE-1-3 Поехали 🚀🧑‍🚀

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,5 +1,18 @@
 package org.example.lesson_1
 
 fun main() {
-    println("Тестовое задание к уроку 1 задаче 3")
+    val yearOfTakeOff: Int = 1961
+    var hourOfTakeOff: Int = 9
+    var minuteOfTakeOff: Int = 7
+
+    println(yearOfTakeOff)
+    println(hourOfTakeOff)
+    println(minuteOfTakeOff)
+
+    hourOfTakeOff = 10
+    minuteOfTakeOff = 55
+
+    print(hourOfTakeOff)
+    print(":")
+    print(minuteOfTakeOff)
 }
