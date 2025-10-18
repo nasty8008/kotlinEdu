47971eb84616ac2191374d9fa85f424cadd09c7d
nasty8008 <nastya901296@gmail.com>
2025-10-18 20:17:38 +0400

    KS-1-3 Изменила тип переменных времени на строки

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,15 +2,15 @@ package org.example.lesson_1
 
 fun main() {
     val yearOfTakeOff: Int = 1961
-    var hourOfTakeOff: Int = 9
-    var minuteOfTakeOff: Int = 7
+    var hourOfTakeOff: String = "09"
+    var minuteOfTakeOff: String = "07"
 
     println(yearOfTakeOff)
     println(hourOfTakeOff)
     println(minuteOfTakeOff)
 
-    hourOfTakeOff = 10
-    minuteOfTakeOff = 55
+    hourOfTakeOff = "10"
+    minuteOfTakeOff = "55"
 
     print(hourOfTakeOff)
     print(":")
