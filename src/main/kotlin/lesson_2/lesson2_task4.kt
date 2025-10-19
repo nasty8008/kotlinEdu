ba2e877a7ff7a0452600c0949c28dd756eacd8f9
nasty8008 <nastya901296@gmail.com>
2025-10-19 18:27:33 +0400

    KS-2-4 Заменила 100 на константу PERCENT_DIVISOR

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,13 +1,14 @@
 package org.example.lesson_2
 
 const val BUFF_PERCENT = 20
+const val PERCENT_DIVISOR = 100
 
 fun main() {
     val cristalOre: Int = 7
     val ironOre: Int = 11
 
-    val buffForCristal = cristalOre * BUFF_PERCENT / 100
-    val buffForIron = ironOre * BUFF_PERCENT / 100
+    val buffForCristal = cristalOre * BUFF_PERCENT / PERCENT_DIVISOR
+    val buffForIron = ironOre * BUFF_PERCENT / PERCENT_DIVISOR
 
     println("Бонус для кристаллической руды: $buffForCristal")
     println("Бонус для железной руды: $buffForIron")
