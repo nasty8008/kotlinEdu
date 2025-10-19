54289bc181d5b86a47603cadc4bec3fdf20ecf53
nasty8008 <nastya901296@gmail.com>
2025-10-19 13:45:00 +0400

    KS-2-3 Изменила входные данные на часы и минуты отдельно

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -3,7 +3,9 @@ package org.example.lesson_2
 const val MINUTES_PER_HOUR = 60
 
 fun main() {
-    val departureTime: Int = 579
+    val departureHours: Int = 9
+    val departureMinutes: Int = 39
+    val departureTime = departureHours * MINUTES_PER_HOUR + departureMinutes
     val travelTime: Int = 457
     val arrivalTime: Int = departureTime + travelTime
 
