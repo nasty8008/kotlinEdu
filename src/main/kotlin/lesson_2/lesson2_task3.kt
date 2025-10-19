12c4bea6e01c83ef5c37bdc70fa2e89d021c2860
nasty8008 <nastya901296@gmail.com>
2025-10-19 13:02:46 +0400

    KS-2-3 Время прибытия поезда

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+const val MINUTES_PER_HOUR = 60
+
+fun main() {
+    val departureTime: Int = 579
+    val travelTime: Int = 457
+    val arrivalTime: Int = departureTime + travelTime
+
+    println("Поезд прибудет в %02d:%02d".format(arrivalTime / MINUTES_PER_HOUR, arrivalTime % MINUTES_PER_HOUR))
+}
