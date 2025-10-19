5d626333c6038ec4d170027dcad0b2091df1f9db
nasty8008 <nastya901296@gmail.com>
2025-10-19 13:51:20 +0400

    KS-2-4 Изменила значение процента; Использовать .toInt() не стала, т.к. теперь все значения целочисленные → результат вычислений тоже

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_2
 
-const val BUFF = 0.2f
+const val BUFF_PERCENT = 20
 
 fun main() {
     val cristalOre: Int = 7
     val ironOre: Int = 11
 
-    val buffForCristal = cristalOre * BUFF
-    val buffForIron = ironOre * BUFF
+    val buffForCristal = cristalOre * BUFF_PERCENT / 100
+    val buffForIron = ironOre * BUFF_PERCENT / 100
 
-    println("Бонус для кристаллической руды: %.0f".format(buffForCristal))
-    println("Бонус для железной руды: %.0f".format(buffForIron))
+    println("Бонус для кристаллической руды: $buffForCristal")
+    println("Бонус для железной руды: $buffForIron")
 }
