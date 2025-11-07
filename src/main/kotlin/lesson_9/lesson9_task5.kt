fe9a79f4c96d0e4b72ea3ad43df95973c5248721
nasty8008 <nastya901296@gmail.com>
2025-11-07 23:51:34 +0400

    KS-9-5 добавила основное решение

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -1,15 +1,22 @@
 package lesson_9
 
-/*
-Напиши программу, которая запрашивает у пользователя пять названий ингредиентов (каждый по отдельности).
-
-- результат должен исключать повторы;
-- список должен выводиться в алфавитном порядке;
-- все элементы списка выведены в одну строку, разделяя слова запятыми;
-- первый элемент должен быть распечатан с заглавной буквы.
- */
+const val NUMBER_OF_INGREDIENTS = 5
 
 fun main() {
-    println("Введите  ингредиентов, каждый с новой строки:")
+    val listOfIngredients = mutableSetOf<String>()
 
+    println("Введите  ингредиентов, каждый с новой строки:")
+    for (i in 1..NUMBER_OF_INGREDIENTS) {
+        print("$i. ")
+        val ingredient = readln()
+        listOfIngredients.add(ingredient)
+    }
+    println(listOfIngredients.sorted().joinToString().capitalize())
+    /*
+    Для функции capitalize() выходит предупреждение fun String.capitalize() is deprecated
+    Нашла на StackOverflow, что это было сделано из-за особенностей некоторых языков:
+    https://stackoverflow.com/questions/74143020/kotlin-why-is-function-deprecated.
+    (Например: The German letter "ß" formerly existed only in lower case.)
+    Я решила оставить её, так как для русского и английского языков она работает без ошибок
+    */
 }
