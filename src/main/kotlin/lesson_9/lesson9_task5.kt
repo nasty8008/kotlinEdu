a1c5872ffb291eac3a2530a53913b141c7172a2d
nasty8008 <nastya901296@gmail.com>
2025-11-09 21:51:38 +0400

    KS-9-5 Добавила в вывод кол-во ингредиентов и заменила capitalize() на replaceFirstChar { it.uppercase() }

--- src/main/kotlin/lesson_9/lesson9_task5.kt
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -5,22 +5,10 @@ const val NUMBER_OF_INGREDIENTS = 5
 fun main() {
     val listOfIngredients = mutableSetOf<String>()
 
-    println("Введите  ингредиентов, каждый с новой строки:")
+    println("Введите $NUMBER_OF_INGREDIENTS ингредиентов, каждый с новой строки:")
     for (i in 1..NUMBER_OF_INGREDIENTS) {
         print("$i. ")
         listOfIngredients.add(readln())
     }
-    println(listOfIngredients.sorted().joinToString().capitalize())
-    /*
-    Для функции capitalize() выходит предупреждение fun String.capitalize() is deprecated.
-    Нашла на StackOverflow, что это было сделано из-за особенностей некоторых языков:
-    https://stackoverflow.com/questions/74143020/kotlin-why-is-function-deprecated.
-    (Например: The German letter "ß" formerly existed only in lower case.)
-    Я решила оставить её, так как для русского и английского языков она работает без ошибок + выглядит более читаемой,
-    нежели альтернатива, которая есть ниже.
-    */
-
-    //println(
-    //        listOfIngredients.sorted().joinToString()
-    //            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(getDefault()) else it.toString() })
+    println(listOfIngredients.sorted().joinToString().replaceFirstChar { it.uppercase() })
 }
