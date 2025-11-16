1f80328f3fc1d2e4a52bcecb21fc28a9ecd18a3d
nasty8008 <nastya901296@gmail.com>
2025-11-17 00:24:31 +0400

    KS-13-3 Убрала ненужный элвис-оператор и добавила фильтрацию компаний в списке

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -3,7 +3,7 @@ package lesson_13
 class TelephoneRecord(
     val name: String,
     val phone: Long,
-    val company: String? = null ?: "<не указано>",
+    val company: String? = null,
 )
 
 fun main() {
@@ -11,13 +11,16 @@ fun main() {
         TelephoneRecord(name = "Анастасия", phone = 9454676454321),
         TelephoneRecord(name = "nasty8008", phone = 3546987643214, company = "Яндекс"),
         TelephoneRecord(name = "ne_nasty8008", phone = 45679800987564, company = "null"),
-        TelephoneRecord(name = "Настя", phone = 234567980875, company = "Авито"),
+        TelephoneRecord(name = "Настя", phone = 234567980875, company = "Яндекс"),
         TelephoneRecord(name = "Ася", phone = 234567898765)
     )
 
     println("Список всех компаний, которые есть в телефонной книге:")
-    telephoneRecords.forEach {
-        println("• ${it.company}")
-    }
+    telephoneRecords
+        .filter { it.company != null && it.company != "null" }
+        .map { it.company }
+        .distinct()
+        .forEach {
+            println("• $it")
+        }
 }
-
