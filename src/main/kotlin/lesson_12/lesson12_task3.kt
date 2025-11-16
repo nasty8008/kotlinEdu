91961a2bc81ecdaba697d638f0e077a0f94261b5
nasty8008 <nastya901296@gmail.com>
2025-11-16 21:00:10 +0400

    KS-12-3 Теперь все значения температур имеют тип Int + поменяла var на val

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -3,15 +3,15 @@ package lesson_12
 const val CELSIUS_KELVIN_OFFSET = 273.15
 
 class WeatherData(kelvinDayTemp: Int, kelvinNightTemp: Int, _isPrecipitationCondition: Boolean = false) {
-    var dayTemp = kelvinDayTemp - CELSIUS_KELVIN_OFFSET
-    var nightTemp = kelvinNightTemp - CELSIUS_KELVIN_OFFSET
-    var isPrecipitationCondition = _isPrecipitationCondition
+    val dayTemp = (kelvinDayTemp - CELSIUS_KELVIN_OFFSET).toInt()
+    val nightTemp = (kelvinNightTemp - CELSIUS_KELVIN_OFFSET).toInt()
+    val isPrecipitationCondition = _isPrecipitationCondition
 
     fun getWeather() {
         println(
             """
-                Температура днём: ${"%.1f".format(dayTemp)}°С
-                Температура ночью: ${"%.1f".format(nightTemp)}°С
+                Температура днём: $dayTemp°С
+                Температура ночью: $nightTemp°С
                 Вероятность осадков: $isPrecipitationCondition
             """.trimIndent()
         )
