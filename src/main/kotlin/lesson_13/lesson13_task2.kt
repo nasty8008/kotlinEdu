6a92774d098f006d691af07d45b9777c6e5cde14
nasty8008 <nastya901296@gmail.com>
2025-11-16 23:04:59 +0400

    KS-13-2 Дополнила класс методом вывода информации и создала его экземпляр

---
+++ src/main/kotlin/lesson_13/lesson13_task2.kt
@@ -0,0 +1,23 @@
+package lesson_13
+
+class Contact(
+    val name: String,
+    val phone: Long,
+    val company: String? = null,
+) {
+    fun getContactInfo() {
+        println(
+            "- Имя: $name\n" +
+            "- Номер: $phone\n" +
+            "- Компания: ${company ?: "<не указано>"}"
+        )
+    }
+}
+
+fun main() {
+    Contact(
+        name = "Анастасия",
+        phone = 89998914371,
+    ).getContactInfo()
+}
+
