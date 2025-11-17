5207ec79a8e05f76c31de9deccc5c5d7baa85ef8
nasty8008 <nastya901296@gmail.com>
2025-11-17 13:59:42 +0400

    KS-14-1 Разные классы судов

---
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -0,0 +1,34 @@
+package lesson_14
+
+open class Liner(
+    val speed: Double = 30.0,
+    val maxCapacity: Int = 100,     // вместительность пассажиров
+    val maxPayload: Double = 50.0,    // грузоподъемность или вместительность в тоннах (надеюсь верно поняла из условия, что это одно и то же)
+    val canBreakIce: Boolean = false,
+) {
+    fun printShipInfo() {
+        println(
+            """
+            • Скорость: ${speed} км/ч
+            • Вместительность: ${maxCapacity} пассажиров
+            • Грузоподъёмность: ${maxPayload} т
+            • Может колоть лёд: $canBreakIce
+        """.trimIndent()
+        )
+    }
+}
+
+class CargoShip() : Liner(speed = 25.0, maxCapacity = 20, maxPayload = 500.0)
+
+class Icebreaker() : Liner(speed = 20.0, maxCapacity = 15, maxPayload = 40.0, canBreakIce = true)
+
+fun main() {
+    println("Лайнер:")
+    Liner().printShipInfo()
+
+    println("Грузовой:")
+    CargoShip().printShipInfo()
+
+    println("Ледокол:")
+    Icebreaker().printShipInfo()
+}
