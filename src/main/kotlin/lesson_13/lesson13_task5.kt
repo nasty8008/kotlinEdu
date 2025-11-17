ae2bd30a25a8fc2c40c28f0149171fdf9c63e6c7
nasty8008 <nastya901296@gmail.com>
2025-11-17 13:11:22 +0400

    KS-13-5 Убрала класс и сделала вывод только названия исключения

--- src/main/kotlin/lesson_13/lesson13_task5.kt
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -1,28 +1,11 @@
 package lesson_13
 
-class PhoneBookContact(
-    val name: String,
-    val phone: Long,
-    val company: String? = null,
-) {
-    fun printContactInfo() {
-        println(
-            "- Имя: $name\n" +
-                    "- Номер: $phone\n" +
-                    "- Компания: ${company ?: "<не указано>"}"
-        )
-    }
-}
-
-// Не совсем понимаю зачем здесь класс, особенно без логики заполнения телефонной книги.
-// Возможно я неправильно поняла задачу
-
 fun main() {
     print("Введите номер телефона: ")
     try {
         val phone = readln().toLong()
-    } catch (e: Exception) {
-        println(e)
+    } catch (e: NumberFormatException) {
+        println("Ошибка: ${e::class.simpleName}")
     }
 }
 
