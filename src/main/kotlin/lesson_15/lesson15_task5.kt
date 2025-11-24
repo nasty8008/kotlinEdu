230b9733b7838680bdcd0b5e044e17f28b325ea7
nasty8008 <nastya901296@gmail.com>
2025-11-24 21:20:08 +0400

    KS-15-5 Переместила логику методов в интерфейсы и заменила ifы на coerceAtMost()

--- src/main/kotlin/lesson_15/lesson15_task5.kt
+++ src/main/kotlin/lesson_15/lesson15_task5.kt
@@ -7,100 +7,58 @@ interface Movable {
 interface TransportPassengers {
     val maxPassengers: Int
     var currentPassengers: Int
-    fun loadPassengers(passengers: Int)
-    fun unloadPassengers(passengers: Int)
-}
 
-interface TransportCargo {
-    val maxCargo: Double
-    var currentCargo: Double
-    fun loadCargo(cargo: Double)
-    fun unloadCargo(cargo: Double)
-}
-
-class Car : Movable, TransportPassengers {
-    override val maxPassengers: Int = 3
-    override var currentPassengers: Int = 0
-
-    override fun loadPassengers(passengers: Int) {
-        if (passengers > maxPassengers) {
-            println("Нельзя посадить $passengers пассажира в легковую машину! Максимум: $maxPassengers")
-            return
-        }
-        if (currentPassengers + passengers > maxPassengers) {
-            println("Не хватает мест! Сейчас: $currentPassengers, хотят посадить: $passengers")
-            return
-        }
-        currentPassengers += passengers
-        println("В легковую машину посадили $passengers пассажира")
+    fun loadPassengers(passengers: Int) {
+        currentPassengers = (currentPassengers + passengers).coerceAtMost(maxPassengers)
+        println("В машину посадили $currentPassengers пассажира")
     }
 
-    override fun unloadPassengers(passengers: Int) {
+    fun unloadPassengers(passengers: Int) {
         if (passengers > currentPassengers) {
             println("Ошибка: не могут выйти $passengers пассажиров! В машине только: $currentPassengers")
             return
         }
         currentPassengers -= passengers
-        println("Из легковой машины вышли $passengers пассажиров")
-    }
-
-    override fun move() {
-        println("Легковая машина едет с $currentPassengers пассажирами")
+        println("Из машины вышли $passengers пассажиров")
     }
 }
 
-class Truck: Movable, TransportPassengers, TransportCargo {
-    override val maxPassengers: Int = 1
-    override var currentPassengers: Int = 0
-    override val maxCargo: Double = 2.0
-    override var currentCargo: Double = 0.0
+interface TransportCargo {
+    val maxCargo: Int
+    var currentCargo: Int
 
-    override fun move() {
-        println("Грузовик едет с $currentPassengers пассажиром и $currentCargo тоннами груза")
+    fun loadCargo(cargo: Int) {
+        currentCargo = (currentCargo + cargo).coerceAtMost(maxCargo)
+        println("В машину загрузили $currentCargo т груза")
     }
 
-    override fun loadPassengers(passengers: Int) {
-        if (passengers > maxPassengers) {
-            println("Нельзя посадить $passengers пассажиров в грузовик! Максимум: $maxPassengers")
-            return
-        }
-        if (currentPassengers + passengers > maxPassengers) {
-            println("Не хватает мест! Сейчас: $currentPassengers, хотят посадить: $passengers")
-            return
-        }
-        currentPassengers += passengers
-        println("В грузовик посадили $passengers пассажира")
-    }
-
-    override fun unloadPassengers(passengers: Int) {
-        if (passengers > currentPassengers) {
-            println("Ошибка: не могут выйти $passengers пассажиров! В грузовике только: $currentPassengers")
-            return
-        }
-        currentPassengers -= passengers
-        println("Из грузовика вышли $passengers пассажиров")
-    }
-
-    override fun loadCargo(cargo: Double) {
-        if (cargo > maxCargo) {
-            println("Нельзя загрузить $cargo т в грузовик! Максимум: $maxCargo т")
-            return
-        }
-        if (currentCargo + cargo > maxCargo) {
-            println("Недостаточно места! Сейчас в грузовике $currentCargo т груза, хотят загрузить: $cargo т")
-            return
-        }
-        currentCargo += cargo
-        println("В грузовик загрузили $cargo т груза")
-    }
-
-    override fun unloadCargo(cargo: Double) {
+    fun unloadCargo(cargo: Int) {
         if (cargo > currentCargo) {
             println("Ошибка: нельзя выгрузить $cargo т груза! В грузовике только: $currentCargo т")
             return
         }
         currentCargo -= cargo
         println("Из грузовика выгрузили $cargo т груза")
+    }
+}
+
+class Car : Movable, TransportPassengers {
+    override val maxPassengers: Int = 3
+    override var currentPassengers: Int = 0
+
+    override fun move() {
+        println("Легковая машина едет с $currentPassengers пассажирами")
+    }
+}
+
+class Truck : Movable, TransportPassengers, TransportCargo {
+    override val maxPassengers: Int = 1
+    override var currentPassengers: Int = 0
+    override val maxCargo: Int = 2
+    override var currentCargo: Int = 0
+
+    override fun move() {
+        println("Грузовик едет с $currentPassengers пассажиром и $currentCargo тоннами груза")
     }
 }
 
@@ -117,9 +75,9 @@ fun main() {
 
     Truck().apply {
         loadPassengers(1)
-        loadCargo(2.0)
+        loadCargo(2)
         move()
-        unloadCargo(2.0)
+        unloadCargo(2)
         unloadPassengers(1)
     }
 }
