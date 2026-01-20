65ac7a2b5d1b7baf70481032896e73495f3e77fa
nasty8008 <nastya901296@gmail.com>
2026-01-20 21:17:29 +0400

    KS-22-2 Исправила опечатку в имени автора

--- src/main/kotlin/lesson_22/lesson22_task1.kt
+++ src/main/kotlin/lesson_22/lesson22_task1.kt
@@ -1,11 +1,11 @@
 package lesson_22
 
 fun main() {
-    val regBook1 = RegularBook("Анна Каренина", "Дев Толстой")
-    val regBook2 = RegularBook("Анна Каренина", "Дев Толстой")
+    val regBook1 = RegularBook("Анна Каренина", "Лев Толстой")
+    val regBook2 = RegularBook("Анна Каренина", "Лев Толстой")
 
-    val dataBook1 = DataBook("Анна Каренина", "Дев Толстой")
-    val dataBook2 = DataBook("Анна Каренина", "Дев Толстой")
+    val dataBook1 = DataBook("Анна Каренина", "Лев Толстой")
+    val dataBook2 = DataBook("Анна Каренина", "Лев Толстой")
 
     println(regBook1 == regBook2) // false, так как в этом случае сравниваются ссылки на объекты в памяти
     println(dataBook1 == dataBook2) // true, потому что у Data класса сравниваются проинициализированные данные, а не ссылки на объекты
