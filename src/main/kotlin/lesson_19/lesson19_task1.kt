9fda753d8f121ae193eb1cb2a78f20143f0f4802
nasty8008 <nastya901296@gmail.com>
2026-01-21 17:31:52 +0400

    KS-19-1 Упростила способ вывода рыбок с помощью использования .entries

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -1,20 +1,16 @@
 package lesson_19
 
-enum class Fish(val title: String, val id: Int) {
-    GUPPY("гуппи", 1),
-    ANGELFISH("скалярия", 2),
-    GOLDFISH("золотая рыбка", 3),
-    SIAM_FIGHT_FISH("петушок", 4),
+enum class Fish(val title: String) {
+    GUPPY("гуппи"),
+    ANGELFISH("скалярия"),
+    GOLDFISH("золотая рыбка"),
+    SIAM_FIGHT_FISH("петушок"),
 }
 
 fun main() {
-    val listOfFishId = listOf(1, 2, 3, 4)
-
     println("Вот каких рыбок вы можете добавить в свой аквариум:")
-    for (i in listOfFishId) when (i) {
-        Fish.GUPPY.id -> println("- " + Fish.GUPPY.title)
-        Fish.ANGELFISH.id -> println("- " + Fish.ANGELFISH.title)
-        Fish.GOLDFISH.id -> println("- " + Fish.GOLDFISH.title)
-        Fish.SIAM_FIGHT_FISH.id -> println("- " + Fish.SIAM_FIGHT_FISH.title)
+    val listOfFish = Fish.entries
+    listOfFish.forEach {
+        println("- " + it.title)
     }
 }
